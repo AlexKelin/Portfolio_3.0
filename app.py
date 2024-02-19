b09da0de43bff231df4ad831569a3bd4ccf9d497
Alex Kelin <117872291+AlexKelin@users.noreply.github.com>
2024-02-19 18:16:16 -0500

    New

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 from flask import Flask, render_template
-import gunicorn
 
 app = Flask(__name__)
-
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template("index.html")
 
-
-if __name__ == '__main__':
-    from waitress import serve
-
-    serve(app, host="0.0.0.0", port=8080)
